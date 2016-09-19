e87c45df57e2b38a4ab2becd30f958dbb2785d0b
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2016-09-19 10:58:24 +0700

    Initial commit

---
+++ app/src/main/java/jirayu/pond/liveat500px/MainApplication.java
@@ -0,0 +1,23 @@
+package jirayu.pond.liveat500px;
+
+import android.app.Application;
+
+import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+
+/**
+ * Created by lp700 on 18/9/2559.
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Contextor.getInstance().init(getApplicationContext());
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
