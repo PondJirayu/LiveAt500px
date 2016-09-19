a451dead8c14b6e32fda8a24f35e0a62afb68d53
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2016-09-19 11:09:53 +0700

    Update

--- app/src/main/java/jirayu/pond/liveat500px/MainApplication.java
+++ app/src/main/java/jirayu/pond/liveat500px/MainApplication.java
@@ -3,6 +3,8 @@ package jirayu.pond.liveat500px;
 import android.app.Application;
 
 import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by lp700 on 18/9/2559.
@@ -12,6 +14,7 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         Contextor.getInstance().init(getApplicationContext());
     }
