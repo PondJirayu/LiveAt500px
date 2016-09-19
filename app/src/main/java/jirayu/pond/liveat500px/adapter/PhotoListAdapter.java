01fddbe4cc06a78539c7d4f641e221ac39d535fc
Jirayu Sumangkhalaphan <pondjirayu36@gmail.com>
2016-09-19 15:33:23 +0700

    Update

---
+++ app/src/main/java/jirayu/pond/liveat500px/adapter/PhotoListAdapter.java
@@ -0,0 +1,32 @@
+package jirayu.pond.liveat500px.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import jirayu.pond.liveat500px.view.PhotoListItem;
+
+/**
+ * Created by lp700 on 19/9/2559.
+ */
+public class PhotoListAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 100;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return new PhotoListItem(parent.getContext());
+    }
+}
